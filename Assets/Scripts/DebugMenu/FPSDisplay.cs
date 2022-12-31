b98673f0ba50ee5d0a440a594f9613984fc6b007
Peter Gomes <pgomes3@student.fitchburgstate.edu>
2022-12-31 04:02:55 -0500

    FPS Counter Updated

--- Assets/Scripts/DebugMenu/FPSDisplay.cs
+++ Assets/Scripts/DebugMenu/FPSDisplay.cs
@@ -6,30 +6,38 @@ using TMPro;
 public class FPSDisplay : MonoBehaviour
 {
     private TextMeshProUGUI fpsDisplayText;
-    private int avgFPS;
 
-    private float updateTimer = 0.25f;
-    private float currentTimer;
+    private int lastFrameIndex;
+    private float[] frameDeltaTimeArray;
+
+    private void Awake()
+    {
+        frameDeltaTimeArray = new float[50];
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         fpsDisplayText = GetComponent<TextMeshProUGUI>();
-        currentTimer = updateTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
-        avgFPS = (int)(1f / Time.unscaledDeltaTime);
+        frameDeltaTimeArray[lastFrameIndex] = Time.unscaledDeltaTime;
+        lastFrameIndex = (lastFrameIndex + 1) % frameDeltaTimeArray.Length;
 
-        //Update the FPS counter every interval
-        if(currentTimer >= updateTimer)
+        fpsDisplayText.text = "FPS: " + Mathf.RoundToInt(CalculateFPS()).ToString();
+    }
+
+    private float CalculateFPS()
+    {
+        float total = 0f;
+        foreach(float deltaTime in frameDeltaTimeArray)
         {
-            fpsDisplayText.text = "FPS: " + avgFPS.ToString();
-            currentTimer = 0f;
+            total += deltaTime;
         }
-        else
-            currentTimer += Time.unscaledDeltaTime;
+
+        return frameDeltaTimeArray.Length / total;
     }
 }
